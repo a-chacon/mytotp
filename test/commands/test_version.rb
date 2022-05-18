127039234192b5fe6bc89b3c6be3ae89c241ff91
a-chacon <andres.ch@protonmail.com>
2022-05-17 20:37:32 -0400

    fix: test of version to be dynamic

--- test/commands/test_version.rb
+++ test/commands/test_version.rb
@@ -8,28 +8,28 @@ module Test
           Dry::CLI.new(Mytotp::Commands).call(arguments: ["version"])
         end
         assert_equal "", err
-        assert_includes out, "0.1.0"
+        assert_includes out, ::Mytotp::VERSION
       end
 
       def test_alias_v
         out = capture_io do
           Dry::CLI.new(Mytotp::Commands).call(arguments: ["v"])
         end
-        assert_includes out[0], "0.1.0"
+        assert_includes out[0], ::Mytotp::VERSION
       end
 
       def test_alias__v
         out = capture_io do
           Dry::CLI.new(Mytotp::Commands).call(arguments: ["-v"])
         end
-        assert_includes out[0], "0.1.0"
+        assert_includes out[0], ::Mytotp::VERSION
       end
 
       def test_alias__version
         out = capture_io do
           Dry::CLI.new(Mytotp::Commands).call(arguments: ["--version"])
         end
-        assert_includes out[0], "0.1.0"
+        assert_includes out[0], ::Mytotp::VERSION
       end
     end
   end
